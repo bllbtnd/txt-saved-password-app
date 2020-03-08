00b506adb9362ac3ed10ee228ed97de4cf5ecad5
Balla Botond <61012751+bllbtnd@users.noreply.github.com>
2020-03-08 10:17:06 +0100

    Add files via upload

---
+++ PW app with saved pw/Main.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PW_app_with_saved_pw
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fd1 = new OpenFileDialog();
+            fd1.Filter = "Text File Only|*.txt";
+            String Fname = "";
+
+            if (fd1.ShowDialog().Equals(DialogResult.OK))
+            {
+                Fname = fd1.FileName;
+
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            
+            
+            this.Close();
+            
+        }
+    }
+}
---
+++ PW app with saved pw/pw.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace PW_app_with_saved_pw
+{
+    public partial class Form1 : Form
+    {
+
+        
+
+
+       
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //kiiras
+            string p = System.IO.File.ReadAllText(@"pass.txt");
+            //feltvizsg
+            if (textBox1.Text == p)
+            {
+                Main m = new Main();
+                m.Show();
+                this.Hide();
+            }
+            //err szoveg
+            else
+            {
+                textBox1.Text = "Try an other pw...";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //exit gomb
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
